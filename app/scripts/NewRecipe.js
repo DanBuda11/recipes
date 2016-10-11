e1c83b88f952c95886822c5bde187e03b2de55fb
Dan Buda <danbuda@gmail.com>
2016-10-10 23:53:54 -0500

    started output upon submit button

--- app/scripts/NewRecipe.js
+++ app/scripts/NewRecipe.js
@@ -11,12 +11,25 @@ export default React.createClass({
 		console.log(ingredient);
 		this.state.ingredients.push('<li>' + ingredient + '</li>');
 		this.updateIngredients();
+		$('.ingInput').val('');
 
 	},
 	handleStep: function() {
 		let step = $('.stepInput').val();
 		this.state.steps.push('<li>' + step + '</li>');
 		this.updateSteps();
+		$('.stepInput').val('');
+	},
+	submit: function() {
+		let ingredients = this.state.ingredients.map((ingredient) => {
+			return '<div>' + ingredient + '</div>';
+		});
+		$('.proof').html('');
+		$('.proof').append(
+			'<div>' + $('.recipeName').val() + '</div>',
+			ingredients
+		);
+
 	},
 	updateIngredients: function() {
 		$('.ingDisplay').html('');
@@ -30,7 +43,7 @@ export default React.createClass({
 		return (
 			<div className="newContainer">
 				<Link className="homeLink" to ="/"><i className="fa fa-long-arrow-left" aria-hidden="true"></i>Home</Link>
-				<input type="text" placeholder="Recipe Name"></input>
+				<input className="recipeName" type="text" placeholder="Recipe Name"></input>
 				<ul className="ingDisplay"></ul>
 				<input className="ingInput" type="text" placeholder="1 cup milk"></input>
 				<button className="ingButton" onClick={this.handleIngredient}>Add Ingredient</button>
@@ -38,16 +51,13 @@ export default React.createClass({
 				<input className="stepInput" type="text" placeholder="Chop onions"></input>
 				<button className="stepButton" onClick={this.handleStep}>Add Step</button>
 				<textarea placeholder="Makes 4 servings. (max 1000 characters)"></textarea>
-				<button className="submitButton">Submit Recipe!</button>
+				<button className="submitButton" onClick={this.submit}>Submit Recipe!</button>
+				<div className="proof"></div>
 			</div>
 		);
 	}
 });
 
-// name, ingredients (button to submit each time), steps (button to submit each time)
-// notes, image, submit button
-
-// add placeholders, required, test before submit
-
-// text areas
+// Need image submission
+// add required, test before submit
 // min/max required lengths
