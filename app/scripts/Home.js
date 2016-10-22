41b5c40ddf0776b2b0ed3185b8a2e3847d830309
Dan Buda <danbuda@gmail.com>
2016-10-22 14:41:46 -0500

    added recipes, started to add filter-by-course

--- app/scripts/Home.js
+++ app/scripts/Home.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router';
 
 export default React.createClass({
 	getInitialState: function() {
-		return {Recipes: Recipes};
+		return {recipes: Recipes};
 	},
 	// componentDidMount: function() {
 	// 	// Recipes.on('update', this.updateRecipes);
@@ -17,8 +17,19 @@ export default React.createClass({
 	// updateRecipes: function() {
 	// 	this.setState({Recipes: Recipes});
 	// },
+	filter: function() {
+		console.log(this.state.recipes);
+		let courses = this.state.recipes.filter((course, i) => {
+			if(course === 'main') {
+				return true;
+			} else {
+				return false;
+			}
+		});
+		this.setState({recipes: courses});
+	},
 	render: function() {
-		const recipes = this.state.Recipes.sort(function(a,b) {
+		const recipes = this.state.recipes.sort(function(a,b) {
 			let nameA = a.name.toLowerCase();
 			let nameB = b.name.toLowerCase();
 			if (nameA < nameB) {
@@ -42,6 +53,13 @@ export default React.createClass({
 				<div className="addNew">
 					<Link to="/new" className="button">Add a Recipe</Link>
 				</div>
+				<label htmlFor="courseFilter">Filter by:</label>
+				<select id="courseFilter" onClick={this.filter}>
+					<option value="main">Main Dishes</option>
+					<option value="sides">Sides</option>
+					<option value="soups">Soups</option>
+					<option value="seasonings">Seasonings</option>
+				</select>
 				<div className="grid">
 					{recipes}
 				</div>
--- app/scripts/Recipes.js
+++ app/scripts/Recipes.js
@@ -18,59 +18,87 @@ export default [
 			"Add olive oil, lemon juice and salt to bowl and mix thoroughly."
 		],
 		image: "tabbouleh.png",
+		course: "sides",
 		notes: "This is a modified version (or at least as close as I could come) to my favorite tabbouleh from a restaurant outside of Detroit."
 	},
 	{
 		name: "Caldo de Pollo",
 		ingredients: [
 			"1 whole chicken (3-4 pounds), cut into large pieces",
-			"8 cups water",
-			"2 tsp salt",
+			"8-10 cups water, depending on size of chicken",
+			"2-3 tsp salt",
 			"1/2 white onion",
 			"4 garlic gloves, peeled",
 			"3 celery branches",
-			"3 large carrots",
-			"2 cilantro branches"
+			"4 large carrots",
+			"2 cilantro branches",
+			"Other vegetables as desired (potatoes, corn on the cob, cabbage, zucchini).",
+			"Avocado, cilantro, diced onion, lime, diced serrano or jalapeno peppers, cooked white rice, chipotle or adobo sauce to garnish."
 		],
 		steps: [
 			"Place chicken, water and salt into a large stock pot at medium-high heat and bring to a boil. Once foam starts forming on the surface, skim & discard.",
 			"After removing foam, add onion, garlic and celery. Partially cover the pot. Turn heat down and gently simmer for about 30 minutes. Do not boil.",
 			"Add the carrots and cilantro. If using other vegetables, add them 15 minutes later. Check chicken for doneness and, once cooked, remove & set aside to cool.",
-			"Keep simmering the broth for about 10 more minutes until carrots are completely cooked."
+			"Keep simmering the broth for about 10 more minutes until carrots are completely cooked.",
+			"When chicken pieces have cooled, shred or cut into small pieces. Dice carrots once cooked and set aside.",
+			"Remove any remaining vegetables and set aside. Drain broth then return to pot, letting settle for 8-10 minutes. Use a large spoon to remove any fat that rises to the top.",
+			"Cut up vegetables and return to pot along with chicken and heat through.",
+			"Serve with garnishes."
 		],
 		image: "caldo.png",
+		course: "soups",
 		notes: ""
 	},
 	{
-		name: "Poop Soup",
+		name: "Tomatoes, Potatoes & Meat",
 		ingredients: [
-			"1 medium turd",
-			"2 apples",
-			"pinch of fart powder"
+			"1 good sized flank steak or top sirloin, cut into small pieces",
+			"1 lb. pork steak or pork chops, cut into small pieces",
+			"8-10 russet potatoes, thinly sliced",
+			"1 28oz can whole tomatoes",
+			"1 medium onion, chopped",
+			"Salt & pepper to taste",
+			"Butter, optional"
 		],
 		steps: [
-			"Make a doody.",
-			"Saute over medium heat with olive oil.",
-			"Add chicken broth",
-			"Eat Poop Soup. Serves 4."
+			"Combine meat, potatoes, onion in large stock pot, add enough water to cook potatoes, ,and mix thoroughly.",
+			"Dot with butter is desired.",
+			"Cover pot and bake at 350 degrees for 1 1/2 hours.",
+			"Cut tomatoes into small pieces and add to pot.",
+			"Heat through and serve."
 		],
 		image: "no-photo.png",
-		notes: ""
+		course: "main",
+		notes: "THE Buda family recipe. The name says it all."
 	},
 	{
-		name: "Poop Soup",
+		name: "Cajun Blackening Dry Rub",
 		ingredients: [
-			"1 medium turd",
-			"2 apples",
-			"pinch of fart powder"
+			"2 1/2 Tbsp. paprika",
+			"1 Tbsp kosher salt",
+			"1 1/2 tsp ground cumin",
+			"1 1/2 tsp dried thyme",
+			"1 tsp black pepper",
+			"1/2 tsp garlic powder",
+			"1/2 tsp ground red pepper"
 		],
 		steps: [
-			"Make a doody.",
-			"Saute over medium heat with olive oil.",
-			"Add chicken broth",
-			"Eat Poop Soup. Serves 4."
+			"Combine all ingredients. Store in an airtight container for up to 2 weeks."
 		],
-		image: "tabbouleh.png",
-		notes: ""
+		image: "no-photo.png",
+		course: "seasonings",
+		notes: "Great to use on white-fleshed fish, chicken breast, shrimp or corn on the cob."
 	}
 ]
+
+// {
+// 	name: "";
+// 	ingredients: [
+// 	],
+// 	steps: [
+// 	],
+// 	image: "",
+// 	course: "",
+// 	notes: ""
+// }
+
